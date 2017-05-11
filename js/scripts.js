13c7aebe36d3ad11fb49a3a22e9186616d484fdb
nbwns <nicolas.bauwens@gmail.com>
2017-05-11 16:22:42 +0200

    solution avec jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-// Empty JS for your own code to be here
+var $table = $("#meals");
+$.getJSON("https://wt-902485dbb4fca4fccee3a0efcde5b34c-0.run.webtask.io/foodmenu")
+.done(function(data){
+	data = JSON.parse(data);
+	console.log(data.length);
+	data.forEach(function(meal){
+		console.log(meal);
+		$table.append("<tr><td>"+meal.name+"</td><td>"+meal.description+"</td><td>"+meal.price+"</td></tr>");
+	});
+});
